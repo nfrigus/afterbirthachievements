5c28d22a6b3a531bee48be2d37353cf27399592f
Søren Fuglede Jørgensen <s@fuglede.dk>
2017-02-05 16:25:35 +0100

    Add custom tooltips
    This should make the site much easier to use, in particular on mobile devices.

--- tmpl/js/main.js
+++ tmpl/js/main.js
@@ -4,4 +4,35 @@ $(document).ready(function () {
             document.location = "/" + $("#username-input").val()
         }
     });
+
+    // Simple mobile-friendly tooltips; adopted (i.e. stolen) from
+    // https://stackoverflow.com/questions/12539006/tooltips-for-mobile-browsers
+    $(".tooltip").attr("data-close", false);
+
+    // Capture mouse click events
+    $(".has-tooltip").click(function () {
+        var $title = $(this).find(".tooltip");
+        if (!$title.attr("data-close"))
+            $title.show();
+        $title.attr("data-close", false);
+    });
+
+    $(".tooltip").click(function () {
+        $(this).attr("data-close", true);
+    });
+
+    $(document).mouseup(function () {
+        $(".tooltip").hide();
+    });
+
+    // Capture mouse enter/leave events
+    $(".has-tooltip").mouseenter(function () {
+        var $title = $(this).find(".tooltip");
+        $title.show();
+    });
+
+    $(".has-tooltip").mouseleave(function () {
+        var $title = $(this).find(".tooltip");
+        $title.hide();
+    });  
 });
